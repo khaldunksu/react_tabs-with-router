7d59379fe469e020dd62d77858fa57125ce54a4d
ksukhaldun <kseniya.khaldun@gmail.com>
2019-12-26 17:54:06 +0300

    Updates

---
+++ precache-manifest.d3cd0ba3286d26f7054dde60dc9f3145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221ba02d0d0ae5953aeb4fcece62e74a",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "43d8817f86e432d0d726",
+    "url": "/react_tabs-with-router/static/css/main.9901682c.chunk.css"
+  },
+  {
+    "revision": "3f543bd219e7c051ef94",
+    "url": "/react_tabs-with-router/static/js/2.30a15857.chunk.js"
+  },
+  {
+    "revision": "43d8817f86e432d0d726",
+    "url": "/react_tabs-with-router/static/js/main.b04992aa.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
